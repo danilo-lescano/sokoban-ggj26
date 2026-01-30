8a4a83f65bfe30b044271992b9ef484ca5bc7dfc
danilo.lima <danilo.lima@elogroup.com.br>
2026-01-30 01:06:49 -0400

    adicionado alguns placeholder de imagens e gerente de fase

---
+++ src/objects/gerente_fase/gerente_fase.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+@export var numero_fase: int = 1
+
+var tile_size: int = 64
+var offset: int = 32
+
+var scene_map := {
+	"J":  { "scene": preload("res://src/objects/player/player.tscn") },
+	"W":  { "scene": preload("res://src/objects/props/wall/bloqueio.tscn") },
+	"C":  { "scene": preload("res://src/objects/props/caixa/caixa.tscn") },
+
+	"MR": { "scene": preload("res://src/objects/props/mask/mask.tscn"), "color": Game.MaskColor.RED },
+	"MG": { "scene": preload("res://src/objects/props/mask/mask.tscn"), "color": Game.MaskColor.GREEN },
+	"MB": { "scene": preload("res://src/objects/props/mask/mask.tscn"), "color": Game.MaskColor.BLUE },
+	"MY": { "scene": preload("res://src/objects/props/mask/mask.tscn"), "color": Game.MaskColor.YELLOW },
+}
+
+func _ready() -> void:
+	var data: Dictionary = load_json("res://data/fases/fase" + str(numero_fase) + ".json")
+	spawn_from_map(data)
+
+func load_json(path: String) -> Dictionary:
+	var file: FileAccess = FileAccess.open(path, FileAccess.READ)
+	var content: String = file.get_as_text()
+	file.close()
+
+	return JSON.parse_string(content)
+
+func spawn_from_map(data: Dictionary) -> void:
+	for key in data.keys():
+		if not scene_map.has(key):
+			push_warning("Sem scene mapeada para: " + key)
+			continue
+
+		var config = scene_map[key]
+		var scene: PackedScene = config["scene"]
+
+		for grid_pos in data[key]:
+			var node = scene.instantiate()
+			node.position = grid_to_world(grid_pos)
+
+			if config.has("color"):
+				node.color = config["color"]
+
+			add_child(node)
+
+
+func grid_to_world(grid_pos: Array) -> Vector2:
+	return Vector2(
+		grid_pos[1] * tile_size + offset,
+		grid_pos[0] * tile_size + offset
+	)
--- src/objects/props/mask/mask.gd
+++ src/objects/props/mask/mask.gd
@@ -1,20 +1,28 @@
 extends Area2D
 
 var cores: Dictionary = {
-    "red mask": 0,
-    "blue mask": 1,
-    "yellow mask": 2,
-    "green mask": 3,
-    "no mask": 4,
+	"red mask": 0,
+	"blue mask": 1,
+	"yellow mask": 2,
+	"green mask": 3,
+	"no mask": 4,
 }
 
 @export var color: Game.MaskColor = Game.MaskColor.RED
-
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _ready() -> void:
-    $Label.text = cores.find_key(color)
 
+	match color:
+		Game.MaskColor.RED:
+			sprite.texture = preload("res://assets/placeholders/aka.png")
+		Game.MaskColor.BLUE:
+			sprite.texture = preload("res://assets/placeholders/ao.png")
+		Game.MaskColor.YELLOW:
+			sprite.texture = preload("res://assets/placeholders/kiiroi.png")
+		Game.MaskColor.GREEN:
+			sprite.texture = preload("res://assets/placeholders/midori.png")
 
 func _on_body_entered(body: Node2D) -> void:
-    if body is Player:
-        (body as Player).equip_new_mask(color)
+	if body is Player:
+		(body as Player).equip_new_mask(color)
